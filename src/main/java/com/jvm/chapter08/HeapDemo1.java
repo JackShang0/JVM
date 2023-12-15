2e35b173926fd4a516be41725a0b0d865d5ad8d1
shangqj <shangqj@si-tech.com.cn>
2023-12-15 17:48:30 +0800

    堆-堆大小设置案例

---
+++ src/main/java/com/jvm/chapter08/HeapDemo1.java
@@ -0,0 +1,19 @@
+package com.jvm.chapter08;
+
+/**
+ * 添加jvm参数：-Xms20m -Xmx20m  初始化堆大小为20M，最大堆空间为20M
+ */
+public class HeapDemo1 {
+
+    public static void main(String[] args) {
+        System.out.println("start...");
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("end...");
+    }
+
+}
