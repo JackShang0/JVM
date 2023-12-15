0bb785b02719acb3014aaa1b14fd777f855cb006
shangqj <shangqj@si-tech.com.cn>
2023-12-15 18:27:56 +0800

    垃圾回收-gc日志细节

---
+++ src/main/java/com/jvm/chapter17/GcLogTest.java
@@ -0,0 +1,22 @@
+package com.jvm.chapter17;
+
+import java.util.ArrayList;
+
+/**
+ * @description:  -Xms60M -Xmx60M -XX:+PrintGCDetails
+ * @author: shangqj
+ * @date: 2023/12/14
+ * @version: 1.0
+ */
+public class GcLogTest {
+    public static void main(String[] args) {
+
+        ArrayList<byte[]> list = new ArrayList<>();
+        for (int i = 0; i < 500; i++) {
+            byte[] bytes = new byte[1024 * 100];
+            list.add(bytes);
+
+        }
+
+    }
+}
